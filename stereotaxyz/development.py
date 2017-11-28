0eac1bb9a9f793c1cbf89c0220d21ffe8f816fbc
Horea Christian <horea.christ@yandex.com>
2017-11-28 02:35:11 +0100

    Moved development function to plotting module
    Added docstring

--- stereotaxyz/development.py
+++ stereotaxyz/development.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from stereotaxyz import skullsweep
-from stereotaxyz.plotting import make_nii
+from stereotaxyz import plotting
 from nilearn.plotting import plot_stat_map, plot_roi, plot_anat
 from os import path
 import matplotlib
@@ -14,49 +14,24 @@ def basic():
 	skullsweep.implant(30,'VTA',df,ax,'orange','best')
 	plt.show()
 
-def template():
+def old():
 	plt.style.use('stereotaxyz.conf')
 	plt.figure()
 	plt.axis('equal')
 	ax = plt.axes()
-	display = plot_anat(
-		anat_img='/home/chymera/ni_data/templates/DSURQEc_40micron_average.nii',
-		display_mode='x',
-		draw_cross=False,
-		cut_coords=(0,),
-		axes=ax,
-		)
-
 	skull_df = df[df['tissue']=='skull']
 	skull_img = make_nii(skull_df, template='~/ni_data/templates/DSURQEc_200micron_average.nii')
-	skullcolor = matplotlib.colors.ListedColormap(['#909090'], name='skullcolor')
-	display.add_overlay(skull_img, cmap=skullcolor)
-
-	target_df = df[df['ID']=='DR']
-	try:
-		target_x = target_df['posteroanterior'].item()
-	except KeyError:
-		target_x = 0
-	try:
-		target_y = target_df['leftright'].item()
-	except KeyError:
-		target_y = 0
-	try:
-		target_z = -target_df['superoinferior'].item()
-	except KeyError:
-		target_z = 0
-
-	target_coords = [(target_y, target_x, target_z)]
-	print(target_coords)
-	display.add_markers(target_coords, marker_color='#E5E520', marker_size=200)
-
-	slope, intercept, pa_in, is_in = skullsweep.implant(30, 'DR', df,
-		ax = False,
-		color = 'c',
-		plot_projections='best',
+	skullcolor = matplotlib.colors.ListedColormap(['#FFFFFF'], name='skullcolor')
+	plot_roi(skull_img,
+		bg_img='/home/chymera/ni_data/templates/DSURQEc_40micron_average.nii',
+		cut_coords=(0,),
+		display_mode='x',
+		draw_cross=False,
+		cmap=skullcolor,
+		axes=ax,
+		alpha=1.0,
 		)
-	insertion_site = [(0, pa_in, is_in)]
-	display.add_markers(insertion_site, marker_color='#FFFFFF', marker_size=200)
-
 	plt.show()
 
+def demo():
+	plotting.co_plot('DR',data_file, angle=30,)
--- stereotaxyz/plotting.py
+++ stereotaxyz/plotting.py
@@ -1,9 +1,102 @@
-from stereotaxyz.skullsweep import *
+import matplotlib
+import matplotlib.pyplot as plt
 import nibabel as nib
 import numpy as np
 import nibabel as nib
 import numpy as np
+from nilearn.plotting import plot_stat_map, plot_roi, plot_anat
 from os import path
+from stereotaxyz import skullsweep
+
+def co_plot(target, skullsweep_data,
+	angle=0,
+	angle_axis='x',
+	custom_style=False,
+	template='~/ni_data/templates/DSURQEc_40micron_average.nii',
+	text_output=False,
+	save_as='',
+	):
+	"""Co-plot of skullsweep data points together with target and best entry point coordinates (as computed based on the skullsweep data and the angle of entry).
+
+	Parameters
+	----------
+
+	target : str or list or tuple
+		Target identifier. Can either be a string (identifying a row via the 'ID' column of the skullsweep_data DataFrame) or a list or tuple of exactly 3 floats giving the y (leftright), x (posteroanterior), and z (superoinferior) coordinates of the target, in this order.
+	skullsweep_data : str or pandas.DataFrame
+		Path to a CSV file or `pandas.DataFrame` object containing skullsweep and optionally target coordinates.
+		The data should include columns named 'ID', 'posteroanterior', 'superoinferior', 'reference', and 'tissue'.
+	angle : float
+		Desired angle of entry.
+	angle_axis : {'x', 'y'}
+		Along which axis the angle should be applied.
+		Currently only 'x' is supported.
+	custom_style : bool
+		Whether to forego the application of a default style.
+	template : str
+		Path to template (generally an anatomical image) to be used as background.
+	text_output : bool
+		Whether to print relevant output (computed enrty point coordinates and recommended implant length) to the command line.
+	save_as : str
+		Path under which to save the output image.
+	"""
+
+
+	template = path.abspath(path.expanduser(template))
+	animal_df = skullsweep.load_data(skullsweep_data)
+
+	skull_df = animal_df[animal_df['tissue']=='skull']
+	skull_img = make_nii(skull_df, template='~/ni_data/templates/DSURQEc_200micron_average.nii')
+	skull_color = matplotlib.colors.ListedColormap(['#909090'], name='skull_color')
+
+	if type(target) is tuple and len(target) == 3:
+		target_coords = [target]
+	elif type(target) is list and len(target) == 3:
+		target_coords = [tuple(target)]
+	else:
+		target_df = animal_df[animal_df['ID']==target]
+		try:
+			target_x = target_df['posteroanterior'].item()
+		except KeyError:
+			target_x = 0
+		try:
+			target_y = target_df['leftright'].item()
+		except KeyError:
+			target_y = 0
+		try:
+			target_z = -target_df['superoinferior'].item()
+		except KeyError:
+			target_z = 0
+		target_coords = [(target_y, target_x, target_z)]
+	if text_output:
+		print(target_coords)
+
+	slope, intercept, pa_in, is_in = skullsweep.implant(angle, 'DR', animal_df,
+		ax = False,
+		color = 'c',
+		plot_projections='best',
+		)
+	insertion_site = [(0, pa_in, is_in)]
+
+	# Start actual plotting:
+	if not custom_style:
+		plt.style.use('stereotaxyz.conf')
+	plt.figure()
+	plt.axis('equal')
+	ax = plt.axes()
+	display = plot_anat(
+		anat_img='/home/chymera/ni_data/templates/DSURQEc_40micron_average.nii',
+		display_mode=angle_axis,
+		draw_cross=False,
+		cut_coords=(0,),
+		axes=ax,
+		alpha=1.0,
+		)
+	display.add_overlay(skull_img, cmap=skull_color)
+	display.add_markers(target_coords, marker_color='#E5E520', marker_size=200)
+	display.add_markers(insertion_site, marker_color='#FFFFFF', marker_size=200)
+	if not save_as:
+		plt.show()
 
 def make_nii(df_slice,
 	template='/home/chymera/ni_data/templates/DSURQEc_200micron_average.nii',
