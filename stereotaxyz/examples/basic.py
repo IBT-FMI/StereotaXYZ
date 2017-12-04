d44da815ea4195648a03dece6c7162f2d6ddad0a
Horea Christian <horea.christ@yandex.com>
2017-12-04 10:39:13 +0100

    Updated API usage

--- stereotaxyz/examples/basic.py
+++ stereotaxyz/examples/basic.py
@@ -2,7 +2,7 @@ from stereotaxyz.skullsweep import *
 
 data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data")
 data_file = path.join(data_dir,'skull_6465.csv')
-df = load_data(data_file, origin='bregma')
+df = load_data(data_file, ultimate_reference='bregma')
 ax = draw_anatomy(df)
 
 implant(35,'VTA',df,ax,'#56B4ED','best')
