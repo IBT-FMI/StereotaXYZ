784795a252031e2c72bef146cf92aeb244ec1cbc
Horea Christian <horea.christ@yandex.com>
2017-12-08 21:33:17 +0100

    3D plotting example

---
+++ stereotaxyz/examples/basic3d.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from os import path
+from stereotaxyz import plotting, skullsweep
+
+data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data")
+data_file = path.join(data_dir,'skull_6465.csv')
+df = skullsweep.load_data(data_file, ultimate_reference='bregma')
+
+t, x,y,z, df = skullsweep.implant_by_angle('VTA', df, yz_angle=45.,)
+entry = [x,y,z]
+plotting.plot_xyz('VTA', df, entry=entry, projection_color='c', save_as='basic3d.png')
