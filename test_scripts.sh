5c456c9ec27d6a8f9144ec7b4cc208839dabec56
Horea Christian <horea.christ@yandex.com>
2017-12-13 17:24:32 +0100

    Improved test calls

--- test_scripts.sh
+++ test_scripts.sh
@@ -1,5 +1,3 @@
-stereotaxyz text example_data/skull_6465.csv DR -y 45
-stereotaxyz plot2d example_data/skull_6465.csv DR -a 45 --save-as plot2d.png
-stereotaxyz plot3d example_data/skull_6465.csv DR -y 45 --save-as plot3d.png
-stereotaxyz plott3d example_data/skull_6465.csv DR -y 45 --save-as plot3d.png
-sterejfs
+stereotaxyz text example_data/skull_6465.csv DR -y 45 &&
+stereotaxyz plot2d example_data/skull_6465.csv DR -a 45 --save-as plot2d.png &&
+stereotaxyz plot3d example_data/skull_6465.csv DR -y 45 --save-as plot3d.png 
