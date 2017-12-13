08e1462008d810c3db28c1a5efcf04330efce8b6
Horea Christian <horea.christ@yandex.com>
2017-12-13 16:07:37 +0100

    Added and testing command line

---
+++ test_scripts.sh
@@ -0,0 +1,3 @@
+stereotaxyz text ~/src/stereotaxyz/example_data/skull_6465.csv DR -y 45
+stereotaxyz plot2d example_data/skull_6465.csv DR -a 45 --save-as plot2d.png
+stereotaxyz plot3d example_data/skull_6465.csv DR -y 45 --save-as plot3d.png
