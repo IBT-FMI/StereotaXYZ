bf22828a0dddbe8d1353273af801c018aa7b2ea8
Horea Christian <horea.christ@yandex.com>
2018-01-05 10:42:06 +0100

    Added example

---
+++ stereotaxyz/examples/basic3d_offset.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from os import path
+from stereotaxyz import plotting, skullsweep
+
+data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data")
+data_file = path.join(data_dir,'skull_6465_offset.csv')
+df = skullsweep.load_data(data_file, ultimate_reference='bregma')
+
+increment, df = skullsweep.insert_by_angle('VTA', df, yz_angle=45.,)
+plotting.xyz(df, 'VTA', yz_angle=45., save_as='basic3d_offset.pdf', insertion_resolution=0.05)
